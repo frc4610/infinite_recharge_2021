354b1f856a5948858c5bce50cd2c2815131e9377
Brady <bradyschieffer@gmail.com>
2021-02-27 14:58:48 -0600

    Added accelerator lower motor ; untested

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -8,6 +8,9 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -15,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Launcher extends SubsystemBase {
   public TalonFX upperFx;
   public TalonFX lowerFx;
+  public CANSparkMax accelMax;
   
 
   /** Creates a new Launcher. */
@@ -22,17 +26,21 @@ public class Launcher extends SubsystemBase {
   {
     upperFx = new TalonFX(0);
     lowerFx = new TalonFX(1);
+    accelMax = new CANSparkMax(2, MotorType.kBrushless);
     
   upperFx.configOpenloopRamp(2);
   lowerFx.configOpenloopRamp(2);
+  accelMax.setOpenLoopRampRate(2);
   upperFx.setNeutralMode(NeutralMode.Coast);
   lowerFx.setNeutralMode(NeutralMode.Coast);
+  accelMax.setIdleMode(IdleMode.kCoast);
   lowerFx.setInverted(true);
 }
 
 public void launch(ControlMode mode,double launchvalue) {
   upperFx.set(mode, launchvalue);
   lowerFx.set(mode, launchvalue);
+  accelMax.set(.5);
   }
   @Override
   public void periodic() {
