34655b072c9afd5413006659d99be25167b1dcdd
Brady <bradyschieffer@gmail.com>
2021-03-06 13:03:47 -0600

    Tested Code

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -15,7 +15,8 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
-public class Launcher extends SubsystemBase {
+public class Launcher extends SubsystemBase 
+{
   public TalonFX upperFx;
   public TalonFX lowerFx;
   public CANSparkMax accelMax;
@@ -24,24 +25,25 @@ public class Launcher extends SubsystemBase {
   /** Creates a new Launcher. */
   public Launcher() 
   {
-    upperFx = new TalonFX(0);
-    lowerFx = new TalonFX(1);
-    accelMax = new CANSparkMax(2, MotorType.kBrushless);
+    upperFx = new TalonFX(5);
+    lowerFx = new TalonFX(4);
+    accelMax = new CANSparkMax(9, MotorType.kBrushless);
     
-  upperFx.configOpenloopRamp(2);
-  lowerFx.configOpenloopRamp(2);
-  accelMax.setOpenLoopRampRate(2);
-  upperFx.setNeutralMode(NeutralMode.Coast);
-  lowerFx.setNeutralMode(NeutralMode.Coast);
-  accelMax.setIdleMode(IdleMode.kCoast);
-  lowerFx.setInverted(true);
-}
-
-public void launch(ControlMode mode,double launchvalue) {
-  upperFx.set(mode, launchvalue);
-  lowerFx.set(mode, launchvalue);
-  accelMax.set(.5);
+    upperFx.configOpenloopRamp(2);
+    lowerFx.configOpenloopRamp(2);
+    accelMax.setOpenLoopRampRate(2);
+    upperFx.setNeutralMode(NeutralMode.Coast);
+    lowerFx.setNeutralMode(NeutralMode.Coast);
+    accelMax.setIdleMode(IdleMode.kCoast);
   }
+
+    public void launch(ControlMode mode,double launchvalue) 
+    {
+    upperFx.set(mode, launchvalue);
+    lowerFx.set(mode, launchvalue);
+    accelMax.set(launchvalue);
+    }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
